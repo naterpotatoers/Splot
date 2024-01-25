bcaf5534af3f4ef969f0c45192430086b16940fc
naterpotatoers <nate.garza@hotmail.com>
2024-01-24 20:12:36 -0800

    :fire: removes bad installed workspace, adds python hello world node

---
+++ ros2/ros2_ws/src/py_pubsub/py_pubsub/publisher_member_function.py
@@ -0,0 +1,24 @@
+# hello world
+
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import String
+
+
+class MinimalPublisher(Node):
+
+    def __init__(self):
+        super().__init__('minimal_publisher')
+        self.get_logger().info('Hello World!')
+
+def main(args=None):
+    rclpy.init(args=args)
+    minimal_publisher = MinimalPublisher()
+    rclpy.spin(minimal_publisher)
+    minimal_publisher.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
