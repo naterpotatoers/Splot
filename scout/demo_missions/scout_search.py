6a51d75445fec33c35869aa21b75f7ee270c55ea
Adrien Carrou <50222631+acarrou@users.noreply.github.com>
2024-05-05 01:48:23 -0700

    Scout deploy (#31)
    * adds perimeter logic to waypoint generator
    
    * adds waypoint focus direction and working parsed perimeters from webserver

--- scout/demo_missions/scout_search.py
+++ scout/demo_missions/scout_search.py
@@ -1,6 +1,8 @@
 import sys
 import time
 import threading
+import requests
+import json
 import subprocess
 sys.path.append('../')
 from modules.Commander import Commander
@@ -9,8 +11,13 @@ from modules.RunSim import RunSim
 from colorama import Fore, Back, Style
 from modules.WaypointGen import WaypointGen
 
+drone_ID = "scout_1"
+backend_url = "http://localhost:5001"
+desc = "Scout 1 at this location"
+
 def print_cpp(line):
     print(Fore.YELLOW + "Received from C++:", Fore.BLUE, f"{line}", Style.RESET_ALL)
+
 
 def read_from_process(process, WaypointGen):
     while True:
@@ -35,6 +42,12 @@ def read_from_process(process, WaypointGen):
                     # Update current GPS or do other processing
                     WaypointGen.current_gps = gps_coordinates
                     print("\nCurrent GPS updated to:", WaypointGen.current_gps)
+                    
+                    # Send GPS coordinates to the backend
+                    coords = {"lat": WaypointGen.current_gps[0], "lng": WaypointGen.current_gps[1]}
+                    scout_data = {"id": f"{drone_ID}", "coords": coords, "desc": f"{desc}"}
+                    requests.post(f"{backend_url}/explored", json=scout_data)
+
             except ValueError as e:
                 print("Error processing GPS coordinates:", e)
 
@@ -56,9 +69,22 @@ print("Running mission")
 # Start the C++ application
 cpp_process = subprocess.Popen(["../MAVSDK/examples/scout_search/build/scout_search", connection], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
 
+# parse the perimeter data that is a list into a list of tuples
+try:
+    response = requests.get(f"{backend_url}/perimeter").json()
+    perimeter = [(point['coords']['lat'], point['coords']['lng']) for point in response]
+    print("Perimeter data:", perimeter)
+except requests.exceptions.RequestException as e:
+    print(f"Request failed: {e}")
+except KeyError:
+    print("Error in data format: expected keys not found")
+except ValueError:
+    print("JSON parsing error")
+
+
 # Create a Commander object to send commands to the C++ application
 command = Commander(cpp_process)
-waypoint_gen = WaypointGen()
+waypoint_gen = WaypointGen(perimeter=[])
 # Start a thread to continuously read the C++ application's output
 threading.Thread(target=read_from_process, args=(cpp_process,waypoint_gen,), daemon=True).start()
 
@@ -67,25 +93,35 @@ print("Waiting for the system to be ready...")
 time.sleep(5)  # Adjust the delay as needed
 print("System is ready")
 
-
 waypoint_gen.get_gps()
 
 # Send takeoff command
 command.takeoff()
 time.sleep(10)  # Wait for takeoff to complete
 
-waypoint_gen.generate_spiral_path()
+
+command.add_waypoint(47.3978897, 8.54551, 10.0, 5.0, True, 270.0)
+command.add_waypoint(47.397887999999995, 8.545460199999999, 10.0, 5.0, True, 90.0)
+command.add_waypoint(47.3977709, 8.5455683, 10.0, 5.0, False)
+command.add_waypoint(47.3978876, 8.5454683, 10.0, 5.0, False)
+
+
+# waypoint_gen.generate_spiral_path()
 # waypoint_gen.generate_grid_path()
 # waypoint_gen.generate_random_path()
 
 time.sleep(5)
-waypoint_gen.run_search_mission(command, 10.0, 2.0, RTL=True)
+# waypoint_gen.run_search_mission(command, 10.0, 2.0, RTL=True)
 
-while True:
-    command.get_status()
-    time.sleep(25)
-    break
-command.return_to_splot(47.398170327054473, 8.5456490218639658)
+command.upload_mission(RTL=True)
+time.sleep(1)
+command.start_mission()
+
+# while True:
+#     command.get_status()
+#     time.sleep(25)
+#     break
+# command.return_to_splot(47.398170327054473, 8.5456490218639658)
 
 while True:
     command.get_status()
--- scout/scout_search/scout_search.cpp
+++ scout/scout_search/scout_search.cpp
@@ -17,6 +17,7 @@ using namespace mavsdk;
 using std::chrono::milliseconds;
 using std::chrono::seconds;
 using std::this_thread::sleep_for;
+bool return_launch = false;
 
 enum Command {
     UPLOAD,
@@ -44,8 +45,7 @@ std::map<std::string, Command> command_map = {
     {"pause", PAUSE},
     {"rtl", RTL},
     {"takeoff", TAKEOFF},
-    {"land", LAND}
-};
+    {"land", LAND}};
 
 Mission::MissionItem make_mission_item(
     double latitude_deg,
@@ -151,7 +151,9 @@ void add_waypoint(
     double latitude_deg,
     double longitude_deg,
     float relative_altitude_m = 10.0f,
-    float speed_m_s = 5.0f)
+    float speed_m_s = 5.0f,
+    int focus = 0,
+    float yaw_deg = 0.0f)
 {
     std::cout << "Adding waypoint..." << std::endl;
     Mission::MissionItem new_waypoint = make_mission_item(
@@ -163,6 +165,13 @@ void add_waypoint(
         20.0f,
         60.0f,
         Mission::MissionItem::CameraAction::None);
+
+    std::cout << "Focus: " << focus << std::endl;
+    std::cout << "Yaw: " << yaw_deg << std::endl;
+    if (focus==1){
+        new_waypoint.yaw_deg = yaw_deg;
+        focus = 0;
+    }
 
     mission_items.push_back(new_waypoint);
 }
@@ -190,8 +199,9 @@ void drone_status(Telemetry& telemetry, Action& action, Mission& mission)
 
     std::pair<Mission::Result, bool> mission_finished = mission.is_mission_finished();
     std::cout << "Mission Finished: " << mission_finished.second << std::endl;
+    std::cout << "Mission Result: " << mission_finished.first << std::endl;
 
-    if (telemetry.in_air() && mission_finished.second) {
+    if (telemetry.in_air() && mission_finished.second && !return_launch) {
         std::cout << "Mission Finished... landing" << std::endl;
         const auto land_result = action.land();
         if (land_result != Action::Result::Success) {
@@ -218,11 +228,13 @@ void process_movement_command(
         case UPLOAD: {
             mission.set_return_to_launch_after_mission(true);
             upload_mission(mission, mission_items);
+            return_launch = true;
             break;
         }
         case UPLOAD_NO_RTL: {
             mission.set_return_to_launch_after_mission(false);
             upload_mission(mission, mission_items);
+            return_launch = false;
             break;
         }
         case STATUS: {
@@ -230,10 +242,19 @@ void process_movement_command(
             break;
         }
         case ADD_WAYPOINT: {
+            int focus;
             double latitude_deg, longitude_deg;
-            float relative_altitude_m, speed_m_s;
-            iss >> latitude_deg >> longitude_deg >> relative_altitude_m >> speed_m_s;
-            add_waypoint(mission, mission_items, latitude_deg, longitude_deg, relative_altitude_m, speed_m_s);
+            float relative_altitude_m, speed_m_s, yaw_deg;
+            iss >> latitude_deg >> longitude_deg >> relative_altitude_m >> speed_m_s >> focus >> yaw_deg;
+            add_waypoint(
+                mission,
+                mission_items,
+                latitude_deg,
+                longitude_deg,
+                relative_altitude_m,
+                speed_m_s,
+                focus,
+                yaw_deg);
             break;
         }
         case CLEAR: {
