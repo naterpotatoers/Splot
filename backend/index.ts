4a3deedf087ff59ba4469291ffe2d6fabc5cc3c0
Nathanael Garza <41933236+naterpotatoers@users.noreply.github.com>
2024-05-04 19:30:56 -0700

    ♻️ Impelments Working API (#27)
    * :sparkles: adds backend to frontend initial connectivity
    
    * :sparkles: adds explored endpoint - beginning to refactor
    
    * :recycle: refactors endpoints to all use routes - perimiter tbd
    
    * :recycle: cleans up api to all use simple route file'

--- backend/index.ts
+++ backend/index.ts
@@ -1,112 +1,30 @@
 import os from "os";
 import cors from "cors";
-import express from "express";
-import { MapData } from "./types";
-import {
-  MOCK_MAP_EXPLORED,
-  MOCK_MAP_MARKERS,
-  MOCK_MAP_PERIMETER,
-  MOCK_MAP_WAYPOINTS,
-} from "./utils/constants";
+import mapDataRoute from "./routes/mapDataRoute";
+import express, { Request, Response } from "express";
 
 const port = 5000;
 const app = express();
 const networkInterfaces = os.networkInterfaces();
 
-let searchPerimeter: Array<MapData> = MOCK_MAP_PERIMETER;
-
-let droneInterestMarker: Array<MapData> = MOCK_MAP_MARKERS;
-let droneTargetWaypoints: Array<MapData> = MOCK_MAP_WAYPOINTS;
-let droneExploredPositions: Array<MapData> = MOCK_MAP_EXPLORED;
-
-let splotInterestMarker: Array<MapData> = MOCK_MAP_MARKERS;
-let splotTargetWaypoints: Array<MapData> = MOCK_MAP_WAYPOINTS;
-let splotExploredPositions: Array<MapData> = MOCK_MAP_EXPLORED;
-
 app.use(cors());
 app.use(express.json({ limit: "2mb" }));
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
+  console.log("GET", req.baseUrl);
   res.send("Splot API");
 });
 
-app.get("/search/perimeter", (req, res) => {
-  console.log("GET /search/perimeter");
-  res.send(searchPerimeter);
-});
+app.use("/perimeter", mapDataRoute);
 
-app.post("/search/perimeter", (req, res) => {
-  searchPerimeter = req.body;
-  console.log("POST /search/perimeter");
-  res.json(searchPerimeter);
-});
+app.use("/splot/markers", mapDataRoute);
+app.use("/splot/explored", mapDataRoute);
+app.use("/splot/interests", mapDataRoute);
+app.use("/splot/waypoints", mapDataRoute);
 
-app.get("/drone/interest", (req, res) => {
-  console.log("GET /drone/interest");
-  res.send(droneInterestMarker);
-});
-
-app.post("/drone/interest", (req, res) => {
-  droneInterestMarker = req.body;
-  console.log("POST /drone/interest");
-  res.json(droneInterestMarker);
-});
-
-app.get("/drone/waypoints", (req, res) => {
-  console.log("GET /drone/waypoints");
-  res.send(droneTargetWaypoints);
-});
-
-app.post("/drone/waypoints", (req, res) => {
-  droneTargetWaypoints = req.body;
-  console.log("POST /drone/waypoints");
-  res.json(droneTargetWaypoints);
-});
-
-app.get("/drone/explored", (req, res) => {
-  console.log("GET /drone/explored");
-  res.send(droneExploredPositions);
-});
-
-app.post("/drone/explored", (req, res) => {
-  droneExploredPositions = req.body;
-  console.log("POST /drone/explored");
-  res.json(droneExploredPositions);
-});
-
-app.get("/splot/interest", (req, res) => {
-  console.log("GET /splot/interest");
-  res.send(splotInterestMarker);
-});
-
-app.post("/splot/interest", (req, res) => {
-  splotInterestMarker = req.body;
-  console.log("POST /splot/interest");
-  res.json(splotInterestMarker);
-});
-
-app.get("/splot/waypoints", (req, res) => {
-  console.log("GET /splot/waypoints");
-  res.send(splotTargetWaypoints);
-});
-
-app.post("/splot/waypoints", (req, res) => {
-  splotTargetWaypoints = req.body;
-  console.log("POST /splot/waypoints");
-  res.json(splotTargetWaypoints);
-});
-
-app.get("/splot/explored", (req, res) => {
-  console.log("GET /splot/explored");
-  res.send(splotExploredPositions);
-});
-
-app.post("/splot/explored", (req, res) => {
-  splotExploredPositions = req.body;
-  console.log("POST /splot/explored");
-  res.json(splotExploredPositions);
-});
+app.use("/scout/explored", mapDataRoute);
+app.use("/scout/waypoints", mapDataRoute);
 
 app.listen(port, () => {
   console.log(`Server: http://localhost:${port}`);
--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,6 +1,36 @@
 import axios from 'axios';
+import { MapReducerState } from '../reducer/mapReducer';
+import { MapData } from '../types';
 
-export async function getExploredPositions() {
+export async function getExploredPositions(): Promise<MapData[]> {
   const response = await axios.get('http://localhost:5000/splot/explored');
   return response.data;
+}
+
+export async function getPerimeterPositions(): Promise<MapData[]> {
+  const response = await axios.get('http://localhost:5000/splot/perimeter');
+  return response.data;
+}
+
+export async function getWaypointPositions(): Promise<MapData[]> {
+  const response = await axios.get('http://localhost:5000/splot/waypoint');
+  return response.data;
+}
+
+export async function getMarkerPositions(): Promise<MapData[]> {
+  const response = await axios.get('http://localhost:5000/splot/marker');
+  return response.data;
+}
+
+export async function getAllMapPositions(): Promise<MapReducerState> {
+  const explored = await getExploredPositions();
+  const perimeter = await getPerimeterPositions();
+  const waypoint = await getWaypointPositions();
+  const marker = await getMarkerPositions();
+  return {
+    explored,
+    perimeter,
+    waypoint,
+    marker,
+  };
 }
